116d8043a19cbaeb817bbaf5a4a23f526a5ad3a1
RogerPellicano <armedandnerdy@gmail.com>
2021-04-03 17:42:40 -0400

    Added Player Movement
    Added player movement & jumping

---
+++ GameDev/Current Plan/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    int speed = 5;
+    [SerializeField]
+    int jumpForce = 5;
+    Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            //move left
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //move right
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //jump            
+            rigidbody2D.AddForce((Vector2.up * jumpForce * Time.deltaTime),ForceMode2D.Impulse);
+        }
+
+    }
+}
---
+++ GameDev/Current Plan/Assets/PlayerPossess.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPossess : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * if(near possessable object & button is pressed)
+         * {
+         *  Possess()
+         * }
+         * */
+    }
+
+    void Possess()
+    {
+        if(/*robot*/true)
+        {
+            //possess robot
+        }
+
+        if (/*wire*/true)
+        {
+            // possess Wire
+        }
+    }
+}
